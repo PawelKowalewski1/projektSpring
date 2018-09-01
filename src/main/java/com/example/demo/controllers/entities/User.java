a54bebf4bf037afd6d1fd7d48442f6efc2e7efdc
jakub.wojcik <jakubwojcik2@gmail.com>
2018-09-01 10:32:29 +0200

    drugiCommit

--- src/main/java/com/example/demo/controllers/entities/User.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.controllers.entities;
-
-public class User {
-
-    private Long id;
-}
---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+    private String GitHubLogin;
+    private String telephone;
+    private Long sqlOcena;
+    private String sqlOpis;
+    private Long pyOcena;
+    private String pyOpis;
+    private Long javaOcena;
+    private String javaOpis;
+    private Long springOcena;
+    private String springOpis;
+
+    @Column(name="enabled")
+    private Boolean active;
+
+
+}
