38f4b721d185f871fe174e154db30ef1089ce0ff
PawelKowalewski1 <p.m.kowalewski@gmail.com>
2018-09-01 12:12:56 +0200

    commit z dodanymi configami

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers.forms;
+package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/register")
     public String rejestracja (Model model){
         return "";
 
     }
-    @PostMapping
+    @PostMapping("/register")
     public String rejestracjaAkcja (Model model){
         return "";
     }
-}
+
+
+    @GetMapping("/login")
+    public String loginForm (){
+
+
+
+
+        return "logowanie";
+    }}
